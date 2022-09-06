e453620348ff252a3033470c254b2a76adef6b1b
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2022-09-06 18:00:34 +0900

    Initial Commit

---
+++ Metronome/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Metronome
+//
+//  Created by iwashita on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by iwashita on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
